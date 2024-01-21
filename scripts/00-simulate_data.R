dc7f5a48eb7590e2e79310df51f50cc2ca350326
Shipeng Zhang <57240985+zero616@users.noreply.github.com>
2024-01-21 17:05:42 -0500

    simulation data and download data updated

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,6 +14,29 @@ library(tidyverse)
 
 #### Simulate data ####
 # [...ADD CODE HERE...]
+#### Simulate ####
+set.seed(853)
+
+# Number of rows in the table
+num_rows <- 100
+
+# Create a vector of years from 2017 to 2023
+years <- rep(seq(2017, 2023), each = num_rows )
+
+# Simulate data for the table
+data <- data.frame(
+  Year.of.death = rep(years, each = num_rows ),
+  Cause_of_death = sample(c("Accident", "Cancer", "Cardiovascular Disease", "Drug Toxicity", "Homicide", "Other", "Pneumonia", "Suicide"), num_rows, replace = TRUE),
+  Age_group = sample(c("40-59", "60+", "Unknown", "20-39", "<20"), num_rows, replace = TRUE),
+  Gender = sample(c("Male", "Female"), num_rows, replace = TRUE),
+  Count = sample(1:10, num_rows, replace = TRUE)
+)
+
+# Add COVID-19 as a cause of death from 2020 onwards
+data$Cause_of_death[data$Year.of.death >= 2020] <- sample(c("Accident", "Cancer", "Cardiovascular Disease", "Drug Toxicity", "Homicide", "Other", "Pneumonia", "Suicide", "Unknown/Pending", "COVID-19"), sum(data$Year.of.death >= 2020), replace = TRUE)
+
+# Print the first few rows of the simulated data
+head(data)
 
 
 
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -8,19 +8,30 @@
 # Any other information needed? [...UPDATE THIS...]
 
 
-#### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
+#### Workspace setup #### 
+library("janitor")
+library("knitr")
+library("lubridate")
+library("opendatatoronto")
+library("tidyverse")
+library(dplyr)
 # [...UPDATE THIS...]
 
 #### Download data ####
 # [...ADD CODE HERE TO DOWNLOAD...]
 
+packages <- list_packages(limit = 20)
+packages
 
+search <- search_packages("homeless")
+find = list_package_resources("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
 
+toronto_homeless_death <- list_package_resources("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")|>
+  dplyr::filter(name == "Homeless deaths by cause.csv") |>
+  get_resource()
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(x=toronto_homeless_death , file = "inputs/data/raw_data.csv") 
 
          
